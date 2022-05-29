e8c47445f2573731478cc1c617855b234e643b0e
Sanoj Silva <silvasanoj@gmail.com>
2022-05-29 16:41:02 +0530

    Add privateemail configuration

--- index.js
+++ index.js
@@ -37,9 +37,9 @@ async function backup() {
 
 async function sendEmail() {
   const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false, // true for 465, false for other ports
+    host: "mail.privateemail.com",
+    port: 465,
+    secure: true, // true for 465, false for other ports
     auth: {
       user: email,
       pass: emailPass,
